3489ffe57f81ed79fbcf5a1259c452be5d11521d
= <terrahen@web.de>
2017-03-21 22:21:03 +0100

    added url generate function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 require('./boot')('Boot');
+
+const deliverer = use('service::deliverer');
+const Url = use('controller/src/Url');
+const url = new Url('command.user.view', {
+  user: 55,
+});
+const request = deliverer.getRequest(url.generate());
+request.execute();
+
+log(url.generate());
